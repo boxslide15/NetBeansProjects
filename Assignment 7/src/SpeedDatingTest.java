2de2ce5691ecc95d197858753408567ea22ba9e3
boxslide15 <luisfhernandez415@gmail.com>
2016-04-25 21:05:00 -0400

    Added user input ( because apparently I lost points for that :/ )

--- Assignment 7/src/SpeedDatingTest.java
+++ Assignment 7/src/SpeedDatingTest.java
@@ -2,6 +2,7 @@
  *
  * @author Luis Hernandez
  */
+import java.util.Scanner;
 public class SpeedDatingTest {
 
     public static void main(String[] args) {
@@ -9,14 +10,33 @@ public class SpeedDatingTest {
         // create a SpeedDating object
         SpeedDating mySpeedDate = new SpeedDating();
 
+        // create scanner object
+        Scanner in = new Scanner(System.in);
+
         // call the happyBirthDaze() method and print results.
-        mySpeedDate.happyBirthDaze(12, 10);
+        int month, day;
+        System.out.print("Please enter your birth month: ");
+        month = in.nextInt();
+        System.out.print("\nPlease enther your birth day: ");
+        day = in.nextInt();
+        mySpeedDate.happyBirthDaze(month, day);
 
         // call and print the pollDancer() method
-        System.out.println("\nElection day is on: " + mySpeedDate.pollDancer(2016).getMediumDate());
+        int year;
+        System.out.print("\nPlease enter a year to calculate when Election Day is held: ");
+        year = in.nextInt();
+        System.out.println("\nElection day is on: " + mySpeedDate.pollDancer(year).getMediumDate());
 
         // Date object for the iExcel() method
-        Date myDate = new Date(4, 7, 2015);
+        int exMonth, exDay, exYear;
+        System.out.print("\nPlease enter the month: ");
+        exMonth = in.nextInt();
+        System.out.print("\nPlease enter the day: ");
+        exDay = in.nextInt();
+        System.out.print("\nPlease enter the year: ");
+        exYear = in.nextInt();
+
+        Date myDate = new Date(exMonth, exDay, exYear);
 
         // Prints the number of days past the excel Date starting from 1/1/1900
         System.out.println("\nThe Excel date for " + myDate.getMediumDate() + " is: #" + mySpeedDate.iExcel(myDate));
